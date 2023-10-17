0d99538a1431e5b1343c3162bc3b6eabe6d08cbd
Aminah <aminahpsaleem@gmail.com>
2023-10-17 15:39:59 +0100

    returns back the user and associated with driver model

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class DriverController extends Controller
 {
-    //
+    public function show(Request $request)
+    {
+        //return back the user and associated driver model
+        $user = $request->user();
+        $user->load('driver');
+        return $user
+    }
+    public function update(Request $request) 
+    {
+
+    }
 }
