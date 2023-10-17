3f4bcc76e121203e85409652dbdb83be42e40a18
Aminah <aminahpsaleem@gmail.com>
2023-10-17 01:57:36 +0100

    creates route for user

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
 
+Route::get('/user', function(Request $request){
+    return $request->user();
+});
