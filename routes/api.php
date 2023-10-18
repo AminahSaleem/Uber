71a528dccf08be34463ba7ca980edb977b5663b2
Aminah <aminahpsaleem@gmail.com>
2023-10-18 15:38:35 +0100

    created routes for the trip controller of the journey starting, ending and its location

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::post('/trip', [TripController::class, 'store']);
     Route::get('/trip/{trip}', [TripController::class, 'show']);
     Route::post('/trip/{trip}/accept', [TripController::class,'accept']);
+    Route::post('/trip/{trip}/accept', [TripController::class,'start']);
+    Route::post('/trip/{trip}/accept', [TripController::class,'end']);
+    Route::post('/trip/{trip}/accept', [TripController::class,'location']);
+
 
     Route::get('/user', function(Request $request){
         return $request->user();
