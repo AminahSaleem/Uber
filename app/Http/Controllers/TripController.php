ed94e9153ce726d953abda7491a1a67b3be69148
Aminah <aminahpsaleem@gmail.com>
2023-10-18 15:20:29 +0100

    adds properties to tripcontroller

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-    //
+    public function store(Request $request){
+        $request->validate([
+            'origin' => 'required',
+            'destination' => 'required',
+            'destination_name' => 'required',
+        ]);
+    }
 }
